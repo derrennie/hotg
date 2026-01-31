d502beb46465f617b38a45b92996b42d6320201e
derrennie <derrennie@gmail.com>
2026-01-31 15:07:01 -0800

    Add files via upload

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '14387895066435956894615998342514464051'                       // used to check whether the localStorage data is stale
+var gzip_hash = '56746972905245398366429187952969237979'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
