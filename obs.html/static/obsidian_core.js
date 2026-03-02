6e39f770c123a007affe8b9e2b8e5358fe5697fe
derrennie <derrennie@gmail.com>
2026-03-01 16:33:06 -0800

    Updated for sessions completed in February.

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '84019423617961790361328548673506396890'                       // used to check whether the localStorage data is stale
+var gzip_hash = '53606985037640222698133359674030156418'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
